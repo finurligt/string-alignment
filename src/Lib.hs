16c1b16a6618a09eab35b3b06c8e25b71abffa65
Fabian Sundholm <fabian.sundholm@gmail.com>
2018-05-12 17:24:55 +0200

    some stuff

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,14 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+similarityScore :: String -> String -> Int
+similarityScore (x:xs) [] = similarityScore xs [] + scoreSpace
+similarityScore [] (y:ys) =  similarityScore ys [] + scoreSpace
+similarityScore [] [] = 0
+similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + if x == y then scoreMatch else scoreMismatch,
+                                      similarityScore xs y:ys + scoreSpace,
+                                      similarityScore x:xs ys + scoreSpace ]
+
+maximaBy :: Ord b => (a -> b) -> [a] -> [a]
+maximaBy :: f xs = foldl () [] xs
